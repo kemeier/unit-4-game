ae4122e8420f2363ee02e1fcf572b7e9798b1988
Kelly Meier <kemeier2000@yahoo.com>
2019-07-21 11:07:26 -0500

    added random numbers for images and goal number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,22 +6,33 @@ $(document).ready(function () {
     var pointsHeading = $("#points");
     var images = $("#images");
 
-    
-    var computerChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+   
     var wins = 0;
     var losses = 0;
     var points = 0;
-    var bunny = 0;
-    var fish = 0;
-    var porcupine = 0;
-    var monkey = 0;
-    var images = [];
-    
-    
-    var randomNumber = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+    var fish = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    var porcupine = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    var monkey = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    var bunny = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+    var randomNumber = (Math.floor(Math.random() * 102) + 19);
     console.log(randomNumber);
 
-    var images = 
+    var fishNumber = fish[Math.floor(Math.random()*fish.length)];
+    console.log(fishNumber);
+
+    var porcupineNumber = porcupine[Math.floor(Math.random()*porcupine.length)];
+    console.log(porcupineNumber);
+
+    var monkeyNumber = monkey[Math.floor(Math.random()*monkey.length)];
+    console.log(monkeyNumber);
+
+    var monkeyNumber = bunny[Math.floor(Math.random()*monkey.length)];
+    console.log(monkeyNumber);
+
+    $("#rdm-number").text(randomNumber);
+   
+    
 
     function resetGame()   {
 
@@ -30,7 +41,7 @@ $(document).ready(function () {
     points = 0;
     
     
-    randomNumberHeading.text(randomNumber);
+   
     winsHeading.text("");
     lossesHeading.text("");
     pointsHeading.text("");
